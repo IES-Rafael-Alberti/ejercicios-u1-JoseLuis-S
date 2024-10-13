48c769931cc0f12087a796e8d6c178cdbb11c496
jsalhor1810 <jsalhor1810@g.educaand.es>
2024-10-13 22:33:39 +0200

    modified:   src/__init__.py     modified:   src/ej1_def.py

--- src/ej1_def.py
+++ src/ej1_def.py
@@ -1,12 +1,9 @@
-# Define la funcion saludo
-def saludo(nom):
-    return 'Hola, ' + nom + '.'
+# Funcion para leer nombre
+def nombre (nom):
+    return nom
 
-# Define main, lee nombre y llama a la funcion saludo
-def main():
-    nombre = input('Introduce tu nombre: ')
-    print(saludo(nombre))
+# Lee el nombre
+nom = input('Introduce tu nombre: ')
 
-# Llama la funcion main
-if __name__ == '__main__':
-    main()
+# Muestra la funcion por pantalla
+print('Hola', nombre(nom)) 
